7ca488cd362229507fcbd6535cf491d5bd3a5dbd
Thada Wangthammang <thada.wth@gmail.com>
2024-08-11 16:02:34 +0700

    feat(react): init project

---
+++ react-vite-staticwebapp-managed-api/apps/api/src/main.ts
@@ -0,0 +1,13 @@
+import { app } from "@azure/functions";
+
+export const hello = app.http("hello", {
+  methods: ["GET", "POST"],
+  authLevel: "function",
+  handler: async (request, context) => {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.get("name") || (await request.text()) || "world";
+
+    return { body: `Hello, ${name}!` };
+  },
+});
